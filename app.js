1ddd53e8f2f821a2ef58b4a552f4991dd6a346a0
patrickhum <jirapat_na@cmu.ac.th>
2023-03-14 14:03:48 +0700

    HTML for login page

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// set the serving static files in the express in a directory named public
+app.use(express.static("public"));
+
+// use bodyParser for URL encoding
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.render('login');
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
